b21e373963743334584b7ac205926dff4be977e6
Francesco Micunco <80624232+FrancescoMicunco@users.noreply.github.com>
2021-10-27 16:13:23 +0200

    Co-authored-by: Asadbek Azamjonov <Asadbek01@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,3 +1,46 @@
-window.onload = function() {
-    fetch("           ").then(e => e.json()).then(data => console.log(data))
-}
+let btnLoadImage = document.getElementById("loadImage")
+divImage = document.querySelector(".image")
+console.log(divImage)
+fetch("https://api.pexels.com/v1/search?query=your-query", {
+        "method": "GET",
+        "mode": 'cors', // no-cors, *cors, same-origin
+        "cache": 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+        "credentials": 'same-origin',
+        "headers": { "Authorization": "563492ad6f917000010000018b84cc9b06444d71933582a04a5a32ce" }
+    }
+
+).then(e => e.json()).then(data => {
+
+    btnLoadImage.addEventListener("click", function() {
+        for (let i = 0; i < data.photos.length; i++) {
+            divImage.innerHTML += `<div class="col-md-4">
+                        <div class="card mb-4 shadow-sm">
+                        <img class="bd-placeholder-img card-img-top" width="100%" height="225" src=${data.photos[i].src.original}>
+    
+                  <title>Placeholder</title>
+                  <rect width="100%" height="100%" fill="#55595c" />
+                  <text x="50%" y="50%" fill="#eceeef" dy=".3em">
+                    Thumbnail
+                  </text>
+                </svg>
+                            <div class="card-body">
+                                <p class="card-text">
+                                    This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.
+                                </p>
+                                <div class="d-flex justify-content-between align-items-center">
+                                    <div class="btn-group">
+                                        <button type="button" class="btn btn-sm btn-outline-secondary">
+                        View
+                      </button>
+                                        <button type="button" class="btn btn-sm btn-outline-secondary">
+                        Edit
+                      </button>
+                                    </div>
+                                    <small class="text-muted">9 mins</small>
+                                </div>
+                            </div>
+                        </div>
+                    </div>`
+        }
+    })
+})
